99259bcf4445342340a86d381a622e91660516cd
Alexey Snytnikov <snytav@gmail.com>
2024-07-06 16:38:12 +0200

    all the 4 matrices OK

--- constrain.py
+++ constrain.py
@@ -20,14 +20,18 @@ def constrain_matrix(A,dof_constrained):
     d = np.max(np.abs(A_ff-A_ff_m))
     #A_ff = np.diag(A[f,f])
 
-    A_fp_m = np.loadtxt('A_fp_cons_m.txt')
     p = np.array(p)
+    A_fp_m = np.loadtxt('A_fp_cons_m.txt')
+    A_fp_m = A_fp_m.reshape(f.shape[0],p.shape[0])
+
     A_fp = A[ix_(f, p)]
     d_fp = np.max(np.abs(A_fp - A_fp_m))
     A_pf = A[ix_(p,f)]
     A_pf_m = np.loadtxt('A_pf_cons_m.txt')
+    A_pf_m = A_pf_m.reshape(p.shape[0],f.shape[0])
     d_pf = np.max(np.abs(A_pf - A_pf_m))
     A_pp = A[ix_(p,p)]
     A_pp_m = np.loadtxt('A_pp_cons_m.txt')
+    A_pp_m = A_pp_m.reshape(p.shape[0],p.shape[0])
     d_pp = np.max(np.abs(A_pp - A_pp_m))
     qq = 0
