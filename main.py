b0d1adf249c6a6265075e321bfa55c73f5b7ac25
Alexey Snytnikov <snytav@gmail.com>
2024-09-08 23:43:36 +0200

    need to make an array of time moments

--- main.py
+++ main.py
@@ -27,12 +27,13 @@ class TimeMoment:
 
 class Element:
     def __init__(self,ndof):
-       self.M = np.zeros((ndof,ndof))
-       self.C = np.zeros((ndof, ndof))
-       self.K = np.zeros((ndof, ndof))
-       self.s = np.zeros(2)
-       self.f = np.zeros(2)
-       self.x = np.zeros(2)
+       self.M    = np.zeros((ndof,ndof))
+       self.C    = np.zeros((ndof, ndof))
+       self.K    = np.zeros((ndof, ndof))
+       self.s    = np.zeros(2)
+       self.f    = np.zeros(2)
+       self.x    = np.zeros(2)
+       self.time = TimeMoment(ndof)
 
 
 
