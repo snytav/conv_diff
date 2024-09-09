07b1e24909bf40f44f83e5557222c18bf0177146
Alexey Snytnikov <snytav@gmail.com>
2024-09-09 23:24:25 +0200

    iterpolation loop OK

--- main.py
+++ main.py
@@ -325,12 +325,12 @@ from interp import interpolation
 A_m = np.loadtxt('A_interp.txt')
 q = A - A_m +np.ones_like(A)
 d_A_m = np.max(q)
-interp_k = np.zeros(n_el)
+interp_nk = np.zeros((n_el,T.shape[0]))
 for n in range(n_el):
     for k in range(T.shape[0]):
         el[n].time[k].u=interpolation(n,time[k].u,A,n_e)
-        time_k_u_m = np.loadtxt('interp_k_' + str(k + 1) + '.txt')
-        interp_k[k] = np.max(np.abs(time_k_u_m - time[k].u))
+        time_k_u_m = np.loadtxt('interp_n_' +str(n+1)+ '_k_' + str(k + 1) + '.txt')
+        interp_nk[n][k] = np.max(np.abs(time_k_u_m - el[n].time[k].u))
 
 
 
