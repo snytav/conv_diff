81821f3ba7a3ec3021bdbf984fa6247cff8317fe
Alexey Snytnikov <snytav@gmail.com>
2024-10-27 23:45:56 +0200

    plot OK

--- conv_diff.py
+++ conv_diff.py
@@ -59,24 +59,7 @@ x
     # 4. organize as function
     # 5. if possible check with some external example (e.g. Lorena Barba's)
     # 6. solve Lavrentiev's problem.
-    plt.figure()
-    # axes('FontSize',14)
-    plt.plot(x_p,u_anal[:,0],label='Initial condition',color='green')
-
-
-
-    xt = np.zeros(n_el)
-    yt = np.zeros(n_el)
-    for n in range(n_el):
-        xt[n] = el[n].x[0]
-        yt[n] = el[n].time[-1].u[0]
-    plt.plot(xt,yt,'o', label='Numerical solution', color='blue')
-    plt.plot(x_p,u_anal[:,-1],label='Analytical solution',color='red')
-    plt.title('Analytical and numerical solution')
-    plt.legend()
-    plt.xlim(x_i,x_f)
-    plt.show(block=True)
-    yt_m = np.loadtxt('yt_final.txt')
-    d_yt = np.max(np.abs(yt-yt_m))
+    from plot_solution import compare_plot
+    xt, yt, d_yt = compare_plot(x_p,u_anal,n_el,el,x_i,x_f)
 
     return xt,yt,d_yt
