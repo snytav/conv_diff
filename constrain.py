2aac6f4fddb625cd1eab701c602c832df1e1ccd8
snytav <snytav@gmail.com>
2024-12-13 17:11:51 +0200

    corrected constrain matrixes

--- constrain.py
+++ constrain.py
@@ -1,8 +1,16 @@
 #function [A_ff,A_fp,A_pf,A_pp]
 import numpy as np
 
+def matrix_by_indices(A,f,p):
+    row, col = np.indices((f.shape[0], p.shape[0]))  # indices for rows and columns
+    c = np.indices(p.shape)  # indices of all columns in row array
+    row[:, c] = f  # assigning f to all the columns of 'row'
+    col = row.T  # we want all the columns and rows with indices from 'f'
+    A_ff = A[row, col]  # getting all the specified elements from 'A'
+    return A_ff
 
 def constrain_matrix(A,dof_constrained):
+    from numpy import ix_
     # Constrain a matrix
     N=A.shape[0]
     p=dof_constrained
@@ -12,15 +20,13 @@ def constrain_matrix(A,dof_constrained):
     f = f_aus - p_aus
     f = np.where(f != 0)
     f = f[0]
+    p = np.array(p)
 
-    A_ff_m = np.loadtxt('A_ff_cons_m.txt')
-    A_ff_m = A_ff_m.reshape(f.shape[0], f.shape[0])
-    row, col = np.indices((f.shape[0], f.shape[0])) # indices for rows and columns
-    c = np.indices(f.shape)    # indices of all columns in row array
-    row[:, c] = f              # assigning f to all the columns of 'row'
-    col = row.T                # we want all the columns and rows with indices from 'f'
-    A_ff = A[row, col]         # getting all the specified elements from 'A'
+
+    A_ff = A[ ix_( f, f ) ]
     #A_ff = np.diag(A[f,f])
-    A_fp = A[f,p]
-    A_pf = A[p,f]
-    A_pp = A[p,p]
+    A_fp = A[ ix_( f, p ) ]
+
+    A_pf = A[ ix_( p, f ) ]
+    A_pp = A[ ix_( p, p ) ]
+    qq = 0
