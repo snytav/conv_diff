a51f60a0db890695c28110dd2cb2e5b659fbfa4a
Alexey Snytnikov <snytav@gmail.com>
2024-12-15 21:50:52 +0200

    final plot  OK

--- data_all.py
+++ data_all.py
@@ -1,7 +1,8 @@
 import numpy as np
+import torch
 
 def data_all_dof(u_f, u_p, dof_constrained):
-    u_f = u_f.detach().numpy()
+    #u_f = u_f.detach().numpy()
     # Data for all DOF
     N = len(u_f) + len(u_p)
     u = np.zeros(N)
@@ -14,6 +15,8 @@ def data_all_dof(u_f, u_p, dof_constrained):
     f = np.where(f != 0)
     f=  f[0]
 
-    u[f.astype(int)] = u_f
+    u[f.astype(int)] = u_f.detach().numpy()
     u[p] = u_p
-    return u
+    upt = torch.from_numpy(u_p)
+    ut = torch.cat((upt[0].reshape(1), u_f, upt[1].reshape(1)))
+    return ut
--- interp.py
+++ interp.py
@@ -1,5 +1,6 @@
 import numpy as np
 from gauss import f_N
+import torch
 
 def interpolation(n,u,A,n_e):
     # Interpolation of scalar variable
@@ -7,9 +8,9 @@ def interpolation(n,u,A,n_e):
     u_e = np.zeros(n_e)
     csi=np.linspace(-1,+1,n_e)
     for i in range(n_e):
-        Ni=f_N(csi[i])
+        Ni=torch.from_numpy(f_N(csi[i]))
         un=u[A[n,:].astype(int)]
-        u_e[i]=np.inner(Ni,un)
+        u_e[i]=torch.inner(Ni,un)
     return u_e
 
 
